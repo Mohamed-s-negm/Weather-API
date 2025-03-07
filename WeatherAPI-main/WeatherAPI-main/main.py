c032f56ac1d3b050761e068a2e834e8388745a64
Mohamed-s-negm <msen6639@gmail.com>
2025-03-07 06:36:03 +0300

    last changes

--- WeatherAPI-main/WeatherAPI-main/main.py
+++ WeatherAPI-main/WeatherAPI-main/main.py
@@ -1,7 +1,35 @@
 import tkinter as tk
 from tkinter import *
+import requests
+import json
 
 FONT_NAME = "Courier"
+API = "98c9329ae008542ccdf45b8822d95097"
+
+def get_weather():
+    # Get city name first
+    city = city_entry.get()
+    # Then use it in the URL
+    URL_L = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=5&appid={API}"
+    
+    response_1 = requests.get(URL_L)
+    location_data = response_1.json()
+    lat = location_data[0]["lat"]
+    lon = location_data[0]["lon"]
+
+    URL_W = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API}&units=metric"
+    response = requests.get(URL_W)
+    weather_data = response.json()
+    temp = weather_data["main"]["temp"]
+    humidity = weather_data["main"]["humidity"]
+    description = weather_data["weather"][0]["description"]
+    wind_speed = weather_data["wind"]["speed"]
+    
+    label_temp.config(text=f"Temperature: {temp}°C")
+    label_humidity.config(text=f"Humidity: {humidity}%")
+    label_description.config(text=f"Description: {description}")
+    label_wind_speed.config(text=f"Wind Speed: {wind_speed} m/s")
+    
 
 window = Tk(screenName="Weather App")
 window.title("Weather App")
@@ -12,20 +40,24 @@ title.pack()
 
 city_label = Label(text="City", font=(FONT_NAME, 16, "bold"))
 city_label.place(x=100, y=70)
+
 city_entry = Entry(width=10)
 city_entry.place(x=100, y=100)
 
-state_code_label = Label(text="State Code", font=(FONT_NAME, 16, "bold"))
-state_code_label.place(x=250, y=70)
-state_code_entry = Entry(width=10)
-state_code_entry.place(x=250, y=100)
+label_temp = Label(text="Temperature", font=(FONT_NAME, 16, "bold"))
+label_temp.place(x=100, y=150)
 
-country_code_label = Label(text="Country Code", font=(FONT_NAME, 16, "bold"))
-country_code_label.place(x=400, y=70)
-country_code_entry = Entry(width=10)
-country_code_entry.place(x=400, y=100)
+label_humidity = Label(text="Humidity", font=(FONT_NAME, 16, "bold"))
+label_humidity.place(x=100, y=200)
 
-start_button = Button(text="Start", font=(FONT_NAME, 16, "bold"))
+label_description = Label(text="Description", font=(FONT_NAME, 16, "bold"))
+label_description.place(x=100, y=250)
+
+label_wind_speed = Label(text="Wind Speed", font=(FONT_NAME, 16, "bold"))
+label_wind_speed.place(x=100, y=300)
+
+
+start_button = Button(text="Start", font=(FONT_NAME, 16, "bold"), command=get_weather)
 start_button.place(x=550, y=150)
 
 
